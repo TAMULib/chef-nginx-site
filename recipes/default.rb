660d55f92b02566894607f01c8d6b00cbd71a77d
Richard Li <evilcat@wisewolfsolutions.com>
2014-04-20 19:23:41 -0500

    working copy

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,4 +3,55 @@
 # Recipe:: default
 #
 # Copyright 2014, Texas A&M University Library
-#
+
+include_recipe "nginx::default" 
+
+# nginx_site 'default' do
+#   enable false
+# end
+file '/etc/nginx/sites-enabled/000-default' do
+  action :delete
+end
+file '/etc/nginx/conf.d/default.conf' do
+  action :delete
+end
+
+directory '/var/run/nginx' do
+  owner node['nginx']['user']
+  group node['nginx']['group']
+end
+
+if node['vhost'].is_a? Hash
+  node['vhost'].each do |name,info|
+    if info.key?('mkdir') and info['mkdir'] 
+      if not File.exists?(info['root'])
+        directory info['root'] do
+          owner node['nginx']['user']
+          group node['nginx']['group']
+          recursive true
+        end
+      elsif not File.directory? info['root']
+        file info['root'] do
+          action :delete
+        end
+        directory info['root'] do
+          owner node['nginx']['user']
+          group node['nginx']['group']
+          recursive true
+        end
+      end
+    end
+    template name do
+      source "vhost.conf.erb"
+      path File.join("/etc/nginx/sites-available/",name)
+      variables(
+        :host => info,
+        :name => name,
+      )
+      notifies :restart, 'service[nginx]', :delayed
+    end
+    nginx_site name do
+      action :enable
+    end
+  end
+end

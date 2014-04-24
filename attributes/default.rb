9e3e4700e20a1f5abd74882f6486ab129502cb3e
Richard Li <yli@library.tamu.edu>
2014-04-23 19:08:45 -0500

    php.ini: timezone

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,4 @@
+default['php']['ini']['timezone'] = 'America/Chicago'
 default['php']['ini']['post_max_size'] = '8M'
 default['php']['ini']['upload_max_filesize'] = '8M'
 default['php']['modules'] = []

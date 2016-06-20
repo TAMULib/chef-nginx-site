03b7ccb772ae8bd0f188da1e890af29a090c09cc
unknown <dhahn@192888SL-E7450.library.tamu.edu>
2016-06-20 11:59:46 -0500

    Dont serve up git folders.

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 name             'nginx-site'
 maintainer       'Texas A&M University Library'
-maintainer_email 'yli@library.tamu.edu'
+maintainer_email 'dhahn@library.tamu.edu'
 license          ''
 description      'Installs/Configures nginx-site'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.9'
+version          '0.3.0'
 depends          'nginx','~> 2.6'
